9a0c918ebe696d62d346fbb5b437520c8beb613b
Kathleen Forgiarini da Silva <82175930+kathleenforgiarini@users.noreply.github.com>
2023-04-12 18:09:16 -0400

    Add files via upload

---
+++ lab8.1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace lab8._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.write = new System.Windows.Forms.Button();
+            this.read = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Code: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Description: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Price: ";
+            // 
+            // write
+            // 
+            this.write.Location = new System.Drawing.Point(21, 162);
+            this.write.Name = "write";
+            this.write.Size = new System.Drawing.Size(92, 49);
+            this.write.TabIndex = 6;
+            this.write.Text = "Write XML file";
+            this.write.UseVisualStyleBackColor = true;
+            this.write.Click += new System.EventHandler(this.write_Click);
+            // 
+            // read
+            // 
+            this.read.Location = new System.Drawing.Point(142, 162);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(92, 49);
+            this.read.TabIndex = 7;
+            this.read.Text = "Read XML file";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(271, 187);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(105, 24);
+            this.exit.TabIndex = 8;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 219);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.read);
+            this.Controls.Add(this.write);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button write;
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ lab8.1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+// CODED BY KATHLEEN FORGIARINI
+// 2023-03-30
+// LAB 8.1
+
+namespace lab8._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        string dir = @".\XML\";
+        private void write_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                if (!Regex.IsMatch(textBox1.Text, @"^[A-Z0-9]{4}$"))
+                {
+                    MessageBox.Show("Product code must be 4 characters of capitalized letters and/or numbers only.");
+                    return;
+                }
+
+                if (!Regex.IsMatch(textBox2.Text, @"^[A-Za-z0-9\s\W]{0,50}$"))
+                {
+                    MessageBox.Show("Description must be up to 50 characters of letters, numbers, and special characters.");
+                    return;
+                }
+
+                decimal price;
+                if (!decimal.TryParse(textBox3.Text, out price) || price < 0 || price > 999.99m)
+                {
+                    MessageBox.Show("Price must be a decimal number representing a value up to 999.99.");
+                    return;
+                }
+
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true; settings.IndentChars = (" ");
+
+                XmlWriter xmlOut = XmlWriter.Create(dir + "Product.xml", settings);
+
+                xmlOut.WriteStartDocument();
+                xmlOut.WriteStartElement("Users");
+                xmlOut.WriteStartElement("Product");
+                xmlOut.WriteElementString("Code", textBox1.Text);
+                xmlOut.WriteElementString("Description", textBox2.Text);
+                xmlOut.WriteElementString("Price", textBox3.Text);
+                xmlOut.WriteEndElement();
+                xmlOut.Close();
+
+                MessageBox.Show("Product data saved to XML file.");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void read_Click(object sender, EventArgs e)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();           
+            settings.IgnoreWhitespace = true;           
+            settings.IgnoreComments = true;           
+            XmlReader xmlIn = XmlReader.Create(dir + "Product.xml", settings);              
+            string tempStr = "", CODE="", DESC="", PRICE="";              
+            if (xmlIn.ReadToDescendant("Product"))           
+            {                 
+                do            
+                {                      
+                    xmlIn.ReadStartElement("Product");                
+                    CODE = xmlIn.ReadElementContentAsString();                     
+                    DESC = xmlIn.ReadElementContentAsString();
+                    PRICE = xmlIn.ReadElementContentAsString();                     
+                    tempStr += CODE + ", " + DESC + ", " + PRICE + "\n";           
+                }              
+                while(xmlIn.ReadToNextSibling("Product"));            
+            }           
+            xmlIn.Close();             
+            MessageBox.Show(tempStr);          
+        }
+    }
+}
